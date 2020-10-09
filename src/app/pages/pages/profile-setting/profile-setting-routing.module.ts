99c718bd8ed206f305b07529ef13c46ca2394876
Vishal <vishal.choudhary@synergytop.com>
2020-10-10 00:08:01 +0530

    profile setting page component is created

---
+++ src/app/pages/pages/profile-setting/profile-setting-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { ProfileComponent } from './profile.component';
+import { VexRoutes } from '../../../../@vex/interfaces/vex-route.interface';
+
+
+const routes: VexRoutes = [
+  {
+    path: '',
+    component: ProfileComponent,
+    data: {
+      toolbarShadowEnabled: true
+    }
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class ProfileRoutingModule {
+}
